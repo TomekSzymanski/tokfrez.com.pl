8fcb7bd81a703d5bd0c3864f8ca2b9bde796c2d8
Tomasz Szymanski <Tomasz.Szymanski@sabre.com>
2019-11-04 13:20:36 +0100

    SEO add favicons

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,6 +40,12 @@ module.exports = function (grunt) {
                         cwd: 'page_admin',
                         src: ['**/*'],
                         dest: 'dist/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'favicons',
+                        src: ['**/*'],
+                        dest: 'dist/'
                     }
                 ]
             }
